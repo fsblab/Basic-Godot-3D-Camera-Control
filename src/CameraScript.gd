f28701cc199445cfdaa6c77498ba52524f3bd40b
fsblab <fsbl@hotmail.de>
2023-10-12 23:18:43 +0200

    minor changes, less unnecessary complexity in code

--- src/CameraScript.gd
+++ src/CameraScript.gd
@@ -14,13 +14,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_key_pressed(KEY_W):
-		transform = transform.translated_local(Vector3.FORWARD * delta * movementSpeed)
+		translate(Vector3.FORWARD * delta * movementSpeed)
 	if Input.is_key_pressed(KEY_A):
-		transform = transform.translated_local(Vector3.LEFT * delta * movementSpeed)
+		translate(Vector3.LEFT * delta * movementSpeed)
 	if Input.is_key_pressed(KEY_S):
-		transform = transform.translated_local(Vector3.BACK * delta * movementSpeed)
+		translate(Vector3.BACK * delta * movementSpeed)
 	if Input.is_key_pressed(KEY_D):
-		transform = transform.translated_local(Vector3.RIGHT * delta * movementSpeed)
+		translate(Vector3.RIGHT * delta * movementSpeed)
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
 		rotation.y -= Input.get_last_mouse_velocity().normalized().x * delta * rotationScalar
--- src/ZoomIOScript.gd
+++ src/ZoomIOScript.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 var speed
-var rootcam
 var mousepos
 
 var viewport
@@ -15,38 +14,40 @@ var zoomScalar
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	speed = 1
-	rootcam = get_node(get_meta("rootCamera"))
 	zoomScalar = 100
+	
+	viewport = get_viewport()
+	cam = viewport.get_camera_3d()
 
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("mmb_up"):
-		viewport = get_viewport()
 		mouse_position = viewport.get_mouse_position()
-		cam = viewport.get_camera_3d()
-		
+
 		direction = cam.project_ray_normal(mouse_position)
-		
-		rootcam.transform = rootcam.transform.translated(direction * zoomScalar * delta)
-	
+		translate(direction * zoomScalar * delta)
+
 	if Input.is_action_just_pressed("mmb_down"):
-		viewport = get_viewport()
 		mouse_position = viewport.get_mouse_position()
-		cam = viewport.get_camera_3d()
-		
+
 		direction = cam.project_ray_normal(mouse_position)
-		
-		rootcam.transform = rootcam.transform.translated(-direction * zoomScalar * delta)
+		translate(-direction * zoomScalar * delta)
+	
+	position.y = clamp(position.y, 0, 20)
 
 
 #func _input(event):
 #	if event is InputEventMouseButton:
 #		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-#			transform = transform.translated_local(Vector3.DOWN * speed)
+#			translate(Vector3.DOWN * zoomScalar * .01)
 #		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-#			transform = transform.translated_local(Vector3.UP * speed)
+#			translate(Vector3.UP * zoomScalar * .01)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+#func _process(delta):
+#	if Input.is_action_just_pressed("mmb_up"):
+#		translate(Vector3.DOWN * delta * zoomScalar)
+#
+#	if Input.is_action_just_pressed("mmb_down"):
+#		translate(Vector3.UP * delta * zoomScalar)
